561635f869a0c3b25c9b4f8d3add6706437ca779
Marçal Peiró Carrión <marcal@221.Xarxa-10-83-98.eduroam.upc.edu>
2016-03-21 19:54:00 +0100

    Master class sample

--- app/www/app/dashboard/dashboard.controller.js
+++ app/www/app/dashboard/dashboard.controller.js
@@ -1,7 +1,9 @@
 angular.module('app.controllers')
-	.controller('DashboardCtrl', ['$scope', '$translate', '$translatePartialLoader',
-		function($scope, $translate, $translatePartialLoader) {
+	.controller('DashboardCtrl', ['$scope', '$translate', '$translatePartialLoader', 'StubsFactory',
+		function($scope, $translate, $translatePartialLoader, StubsFactory) {
 			$translatePartialLoader.addPart('dashboard');
   			$translate.refresh();
+
+            $scope.items = StubsFactory;
 		}
 	]);
--- app/www/app/services.js
+++ app/www/app/services.js
@@ -1,8 +1,19 @@
 angular.module('app.services', [])
 
-// .factory('BlankFactory', [function(){
-
-// }])
+.factory('StubsFactory', ['$http', function($http){
+    return [
+        {
+            name: 'Hammer',
+            imgUrl: 'assets/img/s3H3Hyn0Rn0tv96qQEsc_martillo.jpg',
+            stars: 3
+        },
+        {
+            name: 'Stairs',
+            imgUrl: 'assets/img/SCoBGo4hTsChNUhEKt1B_escalera-madera.jpg',
+            stars: 5
+        }
+    ]
+}])
 
 // .service('BlankService', [function(){
 
