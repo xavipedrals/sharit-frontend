f60d454c6a9f037947d272dc814447d721ce2e3f
Xavier Pedrals <xavier.pedrals.camprubi@est.fib.upc.edu>
2016-03-30 16:07:56 +0200

    Added basic camera feature

--- app/www/app/addProduct/addProduct.controller.js
+++ app/www/app/addProduct/addProduct.controller.js
@@ -2,16 +2,57 @@
  * Created by xavi on 28/03/16.
  */
 angular.module('app.controllers')
-  .controller('AddProductCtrl', ['$scope', '$rootScope', '$translate', '$translatePartialLoader', '$state', 'StubsFactory',
-    function($scope, $rootScope, $translate, $translatePartialLoader, $state, StubsFactory) {
+  .controller('AddProductCtrl', ['$scope',
+    '$rootScope',
+    '$translate',
+    '$translatePartialLoader',
+    '$state', 'StubsFactory',
+    '$cordovaDevice', '$cordovaFile',
+    '$ionicPlatform', '$ionicActionSheet',
+    'ImageService', 'FileService',
+    function($scope, $rootScope, $translate, $translatePartialLoader, $state, StubsFactory, $cordovaDevice, $cordovaFile, $ionicPlatform, $ionicActionSheet, ImageService, FileService) {
       $translatePartialLoader.addPart('addProduct');
       $translate.refresh();
 
       $scope.$state = $state;
       // $scope.product = null;
 
-      $scope.submitProduct = function (product) {
 
+      $ionicPlatform.ready(function() {
+        $scope.images = FileService.images();
+        $scope.$apply();
+      });
+
+      $scope.urlForImage = function(imageName) {
+        var trueOrigin = cordova.file.dataDirectory + imageName;
+        return trueOrigin;
+      };
+
+      $scope.addMedia = function() {
+        $scope.hideSheet = $ionicActionSheet.show({
+          buttons: [
+            { text: 'Take photo' },
+            { text: 'Photo from library' }
+          ],
+          titleText: 'Add images',
+          cancelText: 'Cancel',
+          buttonClicked: function(index) {
+            $scope.addImage(index);
+          }
+        });
+      };
+
+      $scope.addImage = function(type) {
+        $scope.hideSheet();
+        ImageService.handleMediaDialog(type).then(function() {
+          $scope.$apply();
+        });
+      };
+
+
+
+
+      $scope.submitProduct = function (product) {
         this.submittedProduct = product;
         console.log(this.submittedProduct);
         // TODO: Enviar peticio al servidor
--- app/www/app/app.js
+++ app/www/app/app.js
@@ -6,13 +6,14 @@
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('app', [
-  'ionic', 
-  'app.controllers', 
-  'app.routes', 
-  'app.services', 
-  'app.directives', 
+  'ionic',
+  'app.controllers',
+  'app.routes',
+  'app.services',
+  'app.directives',
   'pascalprecht.translate',
-  'app.translation'
+  'app.translation',
+  'ngCordova'
 ])
 
 .run(function($ionicPlatform) {
