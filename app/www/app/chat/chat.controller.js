8788f43fa6104312b76520e14b55a83be77c2cab
Marçal Peiró <marcalpeiro@gmail.com>
2016-04-27 00:57:20 +0200

    public chat worknig

---
+++ app/www/app/chat/chat.controller.js
@@ -0,0 +1,62 @@
+angular.module('app.controllers')
+  .controller('ChatCtrl', ['$scope', '$rootScope', '$translate', '$translatePartialLoader', '$state', 'socket'
+    function($scope, $rootScope, $translate, $translatePartialLoader, $state, socket) {
+      $translatePartialLoader.addPart('chats');
+      $translate.refresh();
+
+      $scope.$state = $state;
+
+      //--chat stuff
+
+
+      socket.forward('message', $scope);
+      $scope.$on('socket:message', function (ev, data) {
+          if ($scope.messages.length > 100) {
+              $scope.messages.splice(0, 1);
+          }
+          var msg = JSON.parse(data);
+          $scope.messages.push(msg);
+
+          /*var hidden = false;
+          if (typeof document.hidden !== "undefined") {
+              hidden = "hidden";
+          } else if (typeof document.mozHidden !== "undefined") {
+              hidden = "mozHidden";
+          } else if (typeof document.msHidden !== "undefined") {
+              hidden = "msHidden";
+          } else if (typeof document.webkitHidden !== "undefined") {
+              hidden = "webkitHidden";
+          }
+
+          // $scope.username is not set if the user didn't provide a name and thus didn't display the chat window
+          // document[hidden] is true if the page is minimized or tabbed-out — details vary by browser
+          if ($scope.username && document[hidden] && msg.type == 'message') {
+              var instance = new Notification(
+                  msg.username + " says:", {
+                       body: msg.message
+                   }
+              );*/
+          }
+      });
+
+      $scope.sendMessage = function () {
+          socket.emit('send_message', $scope.newMessage);
+          $scope.messages.push($scope.newMessage);
+          $scope.newMessage = '';
+      };
+
+      $scope.newRoom = function () {
+                socket.emit('new_room', $scope.selfUsername, $scope.);
+                $scope.messages.push($scope.newMessage);
+                $scope.newMessage = '';
+            };
+
+      $scope.setUsername = function () {
+          $scope.username = $scope.inputUsername;
+          socket.emit('joined_message', $scope.username);
+          // setUsername is called once and can be regarded as "login"
+          Notification.requestPermission(function (permission) {
+          });
+      };
+    }
+  ]);
