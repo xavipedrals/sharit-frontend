c5f61b49238bba3e3c3f4626c232810ec9ea359d
Marçal Peiró <marcalpeiro@gmail.com>
2016-05-20 08:45:43 +0200

    chat working on aws

---
+++ app/www/app/chatRooms/chatRooms.controller.js
@@ -0,0 +1,90 @@
+angular.module('app.controllers')
+    .controller('ChatCtrl',
+        function ($scope, $rootScope, $translate, $translatePartialLoader, $state, socket, $sanitize, $ionicScrollDelegate, $timeout) {
+            $translatePartialLoader.addPart('chats');
+            $translate.refresh();
+
+            $scope.$state = $state;
+
+            var self = this;
+            var typing = false;
+            var lastTypingTime;
+            var TYPING_TIMER_LENGTH = 400;
+
+            self.messages = [];
+
+            var data = new Object();
+            data.userId = "testUser1";
+            data.roomId = "dGVzdFVzZXIxdGVzdFVzZXIyaXRlbTEyMDE2LTA1LTE5IDIwOjUzOjQ0LjAzNTgxNzg0MyArMDIwMCBDRVNU";
+
+
+            socket.on('connection', function () {
+              socket.emit('setRoom', JSON.stringify(data));
+            });
+
+          socket.on('newMessage', function (data) {
+            console.log('newMessage');
+            console.log(data);
+            data = JSON.parse(data);
+            if (data.message && data.userId) {
+              addMessageToList(data.userId, true, data.message)
+            }
+          });
+
+            console.log(JSON.stringify(data));
+            socket.emit('setRoom', JSON.stringify(data));
+
+            //function called when user hits the send button
+            self.sendMessage = function () {
+                data.message = self.message;
+                console.log('send');
+                socket.emit('newMessage', JSON.stringify(data));
+                self.message = ""
+            };
+
+            //function called on Input Change
+            self.updateTyping = function () {
+                sendUpdateTyping()
+            };
+
+            // Display message by adding it to the message list
+            function addMessageToList(username, style_type, message) {
+                username = $sanitize(username);
+                removeChatTyping(username);
+                self.messages.push({
+                    content: $sanitize(message),
+                    style: style_type,
+                    username: username
+                });
+                $ionicScrollDelegate.scrollBottom();
+            }
+
+            // Updates the typing event
+            function sendUpdateTyping() {
+                /*if (!typing) {
+                    typing = true;
+                    socket.emit('typing');
+                }
+                lastTypingTime = (new Date()).getTime();
+                $timeout(function () {
+                    var typingTimer = (new Date()).getTime();
+                    var timeDiff = typingTimer - lastTypingTime;
+                    if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+                        socket.emit('stop typing');
+                        typing = false;
+                    }
+                }, TYPING_TIMER_LENGTH)*/
+            }
+
+            // Adds the visual chat typing message
+            function addChatTyping(username) {
+                addMessageToList(username, true, " is typing");
+            }
+
+            // Removes the visual chat typing message
+            function removeChatTyping(username) {
+                self.messages = self.messages.filter(function (element) {
+                    return element.username != username || element.content != " is typing";
+                })
+            }
+        });
