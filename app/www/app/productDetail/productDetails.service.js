2e603578c65e216c823fa50c3221a3099095f6cc
Joaquín Campos <camposcolmenarejo.joaquin@gmail.com>
2016-05-31 23:03:56 +0200

    Added services for favourites

--- app/www/app/productDetail/productDetails.service.js
+++ app/www/app/productDetail/productDetails.service.js
@@ -1,6 +1,6 @@
 angular.module('app.services')
-	.factory('ProductService', ['$http', 'myConfig',
-		function($http, myConfig) {
+	.factory('ProductService', ['$http', 'myConfig', '$rootScope',
+		function($http, myConfig, $rootScope) {
 			var baseUrl = myConfig.url + ':' + myConfig.port;
 
 			var get = function(id) {
@@ -23,8 +23,54 @@ angular.module('app.services')
 				});
 			};
 
+			var setFavourite = function(id) {
+				return new Promise(function(resolve, reject) {
+					$http({
+						method: 'POST',
+						url: baseUrl + '/fav',
+						data: {
+							'IDitem': id,
+							'IDuser': $rootScope.currentUser.id	// It may be the id of the owner, and not the current user ones.
+																// TODO: Set currentUser as a variable in a service.
+						}
+						// TODO: Capture every request and add the token automatically
+						headers: {'token': window.localStorage.getItem(myConfig.TOKEN_STORAGE_KEY)}
+					})
+					.then(function(response) {
+						resolve(response);
+					}, function(error) {
+						console.log('POST /fav failed: ' + error);
+						reject(error);
+					});
+				});
+			};
+
+			var unsetFavourite = function(id) {
+				return new Promise(function(resolve, reject) {
+					$http({
+						method: 'DELETE',
+						url: baseUrl + '/fav',
+						data: {
+							'IDitem': id,
+							'IDuser': $rootScope.currentUser.id	// It may be the id of the owner, and not the current user ones.
+																// TODO: Set currentUser as a variable in a service.
+						}
+						// TODO: Capture every request and add the token automatically
+						headers: {'token': window.localStorage.getItem(myConfig.TOKEN_STORAGE_KEY)}
+					})
+					.then(function(response) {
+						resolve(response);
+					}, function(error) {
+						console.log('DELETE /fav failed: ' + error);
+						reject(error);
+					});
+				});
+			}
+
 			return {
-				get: get
+				get: get,
+				setFavourite: setFavourite,
+				unsetFavourite: unsetFavourite
 			}
 		}
 	]);
