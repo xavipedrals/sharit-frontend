77f686c7b8bec152c2e054a3bce4a40b461fab4d
Marçal Peiró <marcalpeiro@gmail.com>
2016-06-02 23:45:22 +0200

    valoracions anuncis working

--- app/www/app/chatRooms/chatRooms.controller.js
+++ app/www/app/chatRooms/chatRooms.controller.js
@@ -5,10 +5,23 @@ angular.module('app.controllers')
       $translate.refresh();
 
       $scope.$state = $state;
+      $scope.currentUser = $rootScope.currentUser;
 
       $scope.goToRate = function (room) {
         console.log('RATE')
         console.log(JSON.stringify(room, 1, 1));
+        $rootScope.currentRoom = new Object();
+        $rootScope.currentRoom.id = room.RoomId;
+        $rootScope.currentRoom.userId1= room.UserID1;
+        $rootScope.currentRoom.userName1= room.NameU1;
+        $rootScope.currentRoom.userSurname1= room.SurnameU1;
+        $rootScope.currentRoom.userId2= room.UserID2;
+        $rootScope.currentRoom.userName2= room.NameU2;
+        $rootScope.currentRoom.userSurname2= room.SurnameU2;
+        $rootScope.currentRoom.rated1= Boolean(room.Rated1);
+        $rootScope.currentRoom.rated2= Boolean(room.Rated2);
+        $rootScope.currentRoom.idTrans= room.IdTrans;
+        $rootScope.currentRoom.itemId= room.ItemID;
         $state.go('app.rateProduct');
       }
 
@@ -20,16 +33,15 @@ angular.module('app.controllers')
           url: myConfig.url + ':' + myConfig.port + '/room/findRoom',
           params: {'roomid': room.RoomId}
         }).then(function successCallback(response) {
-          console.log('data data data');
           console.log(JSON.stringify(response, 1, 1));
           $rootScope.currentRoom = new Object();
           $rootScope.currentRoom.id = response.data.Room.RoomId;
           $rootScope.currentRoom.userId1= room.UserID1;
           $rootScope.currentRoom.userName1= room.NameU1;
-          $rootScope.currentRoom.userSurname= room.SurnameU1;
+          $rootScope.currentRoom.userSurname1= room.SurnameU1;
           $rootScope.currentRoom.userId2= room.UserID2;
           $rootScope.currentRoom.userName2= room.NameU2;
-          $rootScope.currentRoom.userSurname= room.SurnameU2;
+          $rootScope.currentRoom.userSurname2= room.SurnameU2;
           $rootScope.currentRoom.messages= response.data.Room.MessagesRoom;
 
           $state.go('app.chat');
@@ -37,7 +49,7 @@ angular.module('app.controllers')
         }, function errorCallback(response) {
           q.reject();
         });
-      }
+      };
 
       var q = $q.defer();
       $http({
--- app/www/app/rateProduct/rateProduct.controller.js
+++ app/www/app/rateProduct/rateProduct.controller.js
@@ -1,9 +1,89 @@
 angular.module('app.controllers')
-.controller('RateProductCtrl', ['$scope', '$rootScope', '$translate', '$translatePartialLoader', '$state', 'NgMap', 'StubsFactory', 'ProfileFactory',
-  function($scope, $rootScope, $translate, $translatePartialLoader, $state, NgMap, StubsFactory, ProfileFactory) {
-    $translatePartialLoader.addPart('rateProduct');
-    $translate.refresh();
-    $scope.$state = $state;
-    
-  }
+  .controller('RateProductCtrl', ['$scope', '$rootScope', '$translate', '$translatePartialLoader', '$state', 'NgMap', 'StubsFactory', 'ProfileFactory', '$q', '$http', 'myConfig',
+    function($scope, $rootScope, $translate, $translatePartialLoader, $state, NgMap, StubsFactory, ProfileFactory, $q, $http, myConfig) {
+      $translatePartialLoader.addPart('rateProduct');
+      $translate.refresh();
+      $scope.$state = $state;
+
+      $scope.rate = function () {
+
+        if ($rootScope.currentUser.id == $rootScope.currentRoom.userId2) {
+          var q = $q.defer();
+          $http({
+            method: 'POST',
+            url: myConfig.url + ':' + myConfig.port + '/valorarUser',
+            params: {
+              'idpet': $rootScope.currentRoom.idTrans,
+              'user': $rootScope.currentRoom.userId1,
+              'roomid': $rootScope.currentRoom.id,
+              'iditem' :$rootScope.currentRoom.itemId,
+              'name': $rootScope.currentRoom.userName1,
+              'surname': $rootScope.currentRoom.userSurname1,
+              'stars': 4,
+              'valoracio': 'Un autentic inutil'
+            },
+            headers: {'token': window.localStorage.getItem('token')}
+          }).then(function successCallback(response) {
+            console.log(JSON.stringify(response,1,1));
+            console.log('User Valorat');
+            $state.go('app.dashboard');
+            q.resolve(response);
+          }, function errorCallback(response) {
+            q.reject();
+          });
+        }/*
+         var data = {
+         name: title,
+         descripcio: description
+         };
+         var q = $q.defer();
+
+         $http({
+         method: 'POST',
+         url: baseUrl + '/peticion',
+         data: data,
+         headers: {'token': window.localStorage.getItem(myConfig.TOKEN_STORAGE_KEY), 'Content-Type': 'application/json'}
+         }).then(function successCallback(response) {
+         console.log("Exito");
+         console.log(response);
+         q.resolve(response);
+         }, function errorCallback(response) {
+         console.log("Puta bida");
+         console.log(response);
+         q.reject();
+         });
+         return q.promise;
+         };
+        */
+        else if ($rootScope.currentUser.id == $rootScope.currentRoom.userId1) {
+          console.log(JSON.stringify($rootScope.currentRoom.idTrans, 1, 1), JSON.stringify($rootScope.currentRoom.userId2,1,1), JSON.stringify($rootScope.currentRoom.id,1,1), $rootScope.currentRoom.itemId, $rootScope.currentRoom.userName2, $rootScope.currentRoom.userSurname2);
+          var q = $q.defer();
+          $http({
+            method: 'POST',
+            url: myConfig.url + ':' + myConfig.port + '/valorarItem',
+            data: {
+              'idpet': $rootScope.currentRoom.idTrans,
+              'user': $rootScope.currentRoom.userId2,
+              'roomid': $rootScope.currentRoom.id,
+              'iditem' :$rootScope.currentRoom.itemId,
+              'name': $rootScope.currentRoom.userName2,
+              'surname': $rootScope.currentRoom.userSurname2,
+              'stars': 4,
+              'valoracio': 'Un autentic inutil'
+            },
+            headers: {'token': window.localStorage.getItem('token')}
+          }).then(function successCallback(response) {
+            console.log(JSON.stringify(response,1,1));
+            console.log('Item Valorat');
+            $state.go('app.dashboard');
+            q.resolve(response);
+          }, function errorCallback(response) {
+            q.reject();
+          });
+        }
+        else {
+          console.log('ERROR: Marçal, ets un inutil. Alguna cosa ha anat malament')
+        }
+      }
+    }
   ]);
